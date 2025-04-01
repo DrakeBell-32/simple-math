ef7c1125f6da85c4f3d1184f5878fda6d0f37138
mkuzin <mkuzin@htsts.ru>
2025-04-01 13:55:18 +0300

    added documentation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { primeFactorization } = require('./src/primeFactorization');
+const { setOfPrimes } = require('./src/setOfPrimes');
 
 module.exports = {
   primeFactorization,
+  setOfPrimes,
 };
